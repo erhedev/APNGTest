ae0dd92f1675ed5e5930102700a09bef9fb828ec
erik.hede.dev@gmail.com <erik.hede.dev@gmail.com>
2019-04-03 10:41:34 +0200

    first

---
+++ APNGTest/Controller/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  APNGTest
+//
+//  Created by Erik Hede on 2019-04-01.
+//  Copyright © 2019 Erik Hede. All rights reserved.
+//
+
+import UIKit
+import APNGKit
+
+class ViewController: UIViewController {
+    
+    var systemInfo = SystemUsageInfo()
+    
+    var pickedImage: APNGImage?
+    
+    var urlString: String?
+    
+    @IBOutlet weak var apngAnimationView: APNGImageView!
+    @IBOutlet weak var cpuUsageLabel: UILabel!
+    @IBOutlet weak var memoryUsageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let apngImage: APNGImage?
+        
+        apngImage = APNGImage(named: "coin-animation")
+        
+        apngAnimationView.image = apngImage
+        
+        apngAnimationView.startAnimating()
+        
+        updateMemoryUsage()
+        updateCPULoad()
+    
+    }
+
+    @IBAction func loadFromLibraryPressed(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        self.present(imagePickerController, animated: true, completion: nil )
+    }
+    
+    @IBAction func loadFromURLPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "URL", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { (textfield) in
+            textfield.placeholder = "URL here:"
+        }
+        
+        let done = UIAlertAction(title: "Done", style: .default) { (action) in
+            if let text = alert.textFields?.first?.text {
+                print(text)
+                self.urlString = text
+            }
+            print("Done")
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("Cancel")
+        }
+        alert.addAction(done)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func updateMemoryUsage() {
+
+        if let memory = systemInfo.report_memory() {
+            memoryUsageLabel.text = String(memory)
+        }
+        
+    }
+    
+    func updateCPULoad() {
+        if let load = systemInfo.hostCPULoadInfo() {
+            cpuUsageLabel.text = String(load)
+        }
+    }
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? APNGImage
+        self.pickedImage = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension APNGImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = APNGImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ APNGTest/Models/SystemUsageInfo.swift
@@ -0,0 +1,55 @@
+//
+//  SystemUsageInfo.swift
+//  APNGTest
+//
+//  Created by Erik Hede on 2019-04-01.
+//  Copyright © 2019 Erik Hede. All rights reserved.
+//
+
+import Foundation
+
+struct SystemUsageInfo {
+    
+    func hostCPULoadInfo() -> Double? {
+        let HOST_CPU_LOAD_INFO_COUNT = MemoryLayout<host_cpu_load_info>.stride/MemoryLayout<integer_t>.stride
+        var size = mach_msg_type_number_t(HOST_CPU_LOAD_INFO_COUNT)
+        var cpuLoadInfo = host_cpu_load_info()
+        
+        let result = withUnsafeMutablePointer(to: &cpuLoadInfo) {
+            $0.withMemoryRebound(to: integer_t.self, capacity: HOST_CPU_LOAD_INFO_COUNT) {
+                host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO, $0, &size)
+            }
+        }
+        
+        let totalTicks = cpuLoadInfo.cpu_ticks.0 + cpuLoadInfo.cpu_ticks.1 + cpuLoadInfo.cpu_ticks.2 + cpuLoadInfo.cpu_ticks.3
+        
+        let sys = Double(cpuLoadInfo.cpu_ticks.0) / Double(totalTicks) * 100
+        
+        if result != KERN_SUCCESS{
+            print("Error  - \(#file): \(#function) - kern_result_t = \(result)")
+            return nil
+        }
+        return sys
+    }
+    
+    func report_memory() -> Int? {
+        var taskInfo = mach_task_basic_info()
+        var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size)/4
+        let kerr: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
+            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+                task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
+            }
+        }
+        
+        if kerr == KERN_SUCCESS {
+            print("Memory used in bytes: \(taskInfo.resident_size)")
+            return Int(taskInfo.resident_size)
+
+        }
+        else {
+            print("Error with task_info(): " +
+                (String(cString: mach_error_string(kerr), encoding: String.Encoding.ascii) ?? "unknown error"))
+            return nil
+        }
+    }
+}
